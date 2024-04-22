8a40a17429cf2c99ab5cf9731d946a932f3a22db
Laukendra Singh <lingh@ezcorp.com>
2024-04-22 19:43:20 +0530

    version change 11

--- src/main/java/com/impressico/lnd/alert/Alert.java
+++ src/main/java/com/impressico/lnd/alert/Alert.java
@@ -1,9 +1,9 @@
 package com.impressico.lnd.alert;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(schema = "alert")
